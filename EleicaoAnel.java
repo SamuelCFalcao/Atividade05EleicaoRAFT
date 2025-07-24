9dfedad1e6da616d9558a70464858a9353ad26ee
SamuelCFalcao <samuelfalcao@alu.ufc.br>
2025-07-24 18:45:22 -0300

    Update EleicaoAnel

--- EleicaoAnel.java
+++ EleicaoAnel.java
@@ -1,52 +1,27 @@
-import java.util.*;
+public class EleicaoAnel
+{
+    public static void main(String[] args) 
+    {
+        int[] processos = {1, 2, 3, 4, 5};
+        boolean[] ativos = {true, true, true, true, false};
 
-class Processo {
-    int id;
-    boolean ativo;
-
-    Processo(int id) {
-        this.id = id;
-        this.ativo = true;
-    }
-}
-
-public class EleicaoSimples {
-    static List<Processo> processos = new ArrayList<>();
-    static int coordenador = -1;
-
-    public static void main(String[] args) {
-        // Criar 5 processos com IDs 1 a 5
-        for (int i = 1; i <= 5; i++) {
-            processos.add(new Processo(i));
-        }
-
-        coordenador = 5; // Processo com maior ID é o coordenador inicial
-        System.out.println("Coordenador inicial: P" + coordenador);
-
-        // Simula falha no coordenador
-        processos.get(4).ativo = false; // ID 5 -> índice 4
-        System.out.println("P5 falhou!");
-
-        // Processo 2 (ID 3) inicia eleição
-        iniciarEleicao(3);
-    }
-
-    public static void iniciarEleicao(int idIniciador) {
-        System.out.println("P" + idIniciador + " iniciou a eleição.");
+        System.out.println("Coordenador original (P5) falhou");
 
         int novoCoordenador = -1;
+        System.out.println("P2 iniciou a eleição.");
 
-        // Verifica todos os processos ativos
-        for (Processo p : processos) {
-            if (p.ativo) {
-                System.out.println("P" + p.id + " está ativo.");
-                if (p.id > novoCoordenador) {
-                    novoCoordenador = p.id;
+        for (int i = 0; i < processos.length; i++) 
+        {
+            if (ativos[i]) 
+            {
+                System.out.println("P" + processos[i] + " está ativo.");
+                if (processos[i] > novoCoordenador) 
+                {
+                    novoCoordenador = processos[i];
                 }
             }
         }
 
-        coordenador = novoCoordenador;
-        System.out.println("Novo coordenador eleito: P" + coordenador);
+        System.out.println("Novo coordenador eleito: P" + novoCoordenador);
     }
 }
